43d5b13f12491097fe83f8f66ec1910682bf05a0
Alessandro Caliaro <acaliaro@libero.it>
2020-03-16 07:22:33 +0100

    first commit

---
+++ FeitianSmartcardReaderTest/MainActivity.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Com.Ftsafe;
+using Com.Ftsafe.ReaderScheme;
+
+namespace FeitianSmartcardReaderTest
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        Handler _handler;
+        private readonly string TAG = "MainActivity";
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+
+            _handler = new MyHandler(HandlerHandleMessage);
+        }
+
+        protected void HandlerHandleMessage(Message msg)
+        {
+            switch (msg.What)
+            {
+                case DK.UsbIn:
+                    Log.Debug(TAG, "UsbIn");
+                    break;
+            }
+        }
+
+        class MyHandler : Handler
+        {
+            public MyHandler(Action<Message> handler)
+            {
+                this.handle_message = handler;
+            }
+
+            Action<Message> handle_message;
+            public override void HandleMessage(Message msg)
+            {
+                handle_message(msg);
+            }
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View) sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+
+            FTReader fTReader = new FTReader(this, _handler, DK.FtreaderTypeUsb);
+
+
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+	}
+}
+
